a0e0045628e38d1f3e5eede5f597c0fe235fd30e
ZennoPy <zennopy@yandex.ru>
2017-08-22 11:42:23 +0600

    Добавлены функции по поиску самого большого и самого маленького баров

--- bars.py
+++ bars.py
@@ -1,21 +1,33 @@
 import json
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_json_data(filepath):
+    with open(filepath, 'r', encoding='cp1251') as file_handler:
+        return json.load(file_handler)
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(json_data):
+    return max(json_data, key=lambda bars:bars['SeatsCount'])['Name']
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(json_data):
+    return min(json_data, key=lambda bars: bars['SeatsCount'])['Name']
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(json_data, longitude, latitude):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        if not len(sys.argv) > 1:
+            filepath = input("Укажите путь к файлу: ")
+            sys.exit()
+        json_content = load_json_data(sys.argv[1])
+        print("Самый большой бар - " + get_biggest_bar(json_content))
+        print("Самый маленький бар - " + get_smallest_bar(json_content))
+    except IndexError:
+        print("Do not specify the path to the file")
+    except FileNotFoundError:
+        print("The specified file was not found")
